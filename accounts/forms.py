2e28dde325f1fce23429a80cbffdede2e9314aaf
unknown <thungan16092003@gmail.com>
2023-10-12 21:05:48 +0700

    Restaurant profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User,UserProfile
 
 
 class UserForm(forms.ModelForm):
@@ -18,3 +18,14 @@ class UserForm(forms.ModelForm):
       raise forms.ValidationError(
         "Password does not match!"
       )
+    
+class UserProfileForm(forms.ModelForm):
+  address=forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Start typing...','required':'required'}))
+  profile_picture=forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+  cover_photo=forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+
+  latitude=forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+  longitude=forms.CharField(widget=forms.TextInput(attrs={'readonly':'readonly'}))
+  class Meta:
+    model=UserProfile 
+    fields=['profile_picture','cover_photo','address','country','state','city','pin_code','latitude','longitude']
